40f97c08fcecf4a9b17e03d2a880f6a3b2ec1b60
fxsym <fsyamsudin24@gmail.com>
2025-04-06 14:54:38 +0700

    Membuat function untuk get data pada seluruh tabel sudah berhasil, get data pada tabel dibuat di controller masing-masing tabel

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function getCategory() {
+        $category = Category::with(['todos'])->get();
+        return $category;
+    }
 }
