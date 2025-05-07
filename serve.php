84683e99c45c92c46510567ed99ed72ce7959dbe
fxsym <fsyamsudin24@gmail.com>
2025-05-07 19:58:45 +0700

    Menambahkan file serve.php untuk hosting

---
+++ serve.php
@@ -0,0 +1,13 @@
+<?php
+
+// public router file
+// Redirect all requests to public/index.php
+if (php_sapi_name() == 'cli-server') {
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . '/public' . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require_once __DIR__ . '/public/index.php';
